d258b65c1b00caab934bd58f978c9ab43ae6542a
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2015-04-23 11:53:45 +0200

    bag of dicks

--- ex3_support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ ex3_support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -18,40 +18,35 @@
 #define RIGHT 4
 #define DOWN 8
 
-int main(int argc, char *argv[])
-{
-	printf("Hello Overlord, input and display test\n");
 
-	uint8_t map[MAP_WIDTH][MAP_HEIGHT];
-	memset(map, 0, sizeof map);
-	
-	uint16_t snakePos[MAP_SIZE] = {261, 260, 259, 258};
-	uint16_t snakeLen = 4;
-	uint8_t snakeDir = RIGHT;
-	uint16_t prevtailPos;
+// DIRTY DIRTY GLOBALS //
+uint8_t map[MAP_WIDTH][MAP_HEIGHT];
 
-	uint8_t foodx = 9;
-	uint8_t foody = 8;  
-	uint8_t foodEaten = 0;
+uint16_t snakePos[MAP_SIZE] = {261, 260, 259, 258};
+uint16_t snakeLen = 4;
+uint8_t snakeDir = RIGHT;
+uint16_t prevtailPos;
 
-	srand(time(NULL));	
-	
-	// function for placing snake on map
-	void placeSnake(void){			 
-		uint16_t x;
-		uint16_t y;
-		for(int i = 0; i < snakeLen; i++){
-			x = snakePos[i]%32;
-			y = snakePos[i]/32;
-			map[x][y] = 1;
-		}
-		x = prevtailPos%32;
-		y = prevtailPos/32;
-		map[x][y] = 0;
+static uint8_t foodx = 9;
+static uint8_t foody = 8;  
+uint8_t foodEaten = 0;
+
+// function for placing snake on map
+void placeSnake(void){			 
+	uint16_t x;
+	uint16_t y;
+	for(int i = 0; i < snakeLen; i++){
+		x = snakePos[i]%32;
+		y = snakePos[i]/32;
+		map[x][y] = 1;
 	}
+	x = prevtailPos%32;
+	y = prevtailPos/32;
+	map[x][y] = 0;
+}
 
-	// function for placing food on map
-	void placeFood(void){
+// function for placing food on map
+void placeFood(void){
 	if(foodEaten == 1){
 		foodx = rand() % (MAP_WIDTH-1);
 		foody = rand() % (MAP_HEIGHT-1);
@@ -60,85 +55,93 @@ int main(int argc, char *argv[])
 		foodEaten = 0;
 	}	
 	map[foodx][foody] = 2;
-		for(int y = 0; y < MAP_HEIGHT; y++){
-			for(int x = 0; x < MAP_WIDTH; x++){
-				if(map[x][y] == 2){
-					drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, GREEN);
-				}
+	for(int y = 0; y < MAP_HEIGHT; y++){
+		for(int x = 0; x < MAP_WIDTH; x++){
+			if(map[x][y] == 2){
+				drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, GREEN);
 			}
-		}	
+		}
 	}	
+}
+
+// function for moving snake	
+void moveSnake(uint8_t direction){
+		
+	prevtailPos = snakePos[snakeLen-1];
+
+	for(int i = MAP_SIZE; i > 0; i--){
+		if(i < snakeLen){
+			snakePos[i] = snakePos[i-1]; 	
+		}
+		else{
+			snakePos[i] = 0;
+		}
+	}
+//	for(int i=0;i<snakeLen;i++){
+//		printf("snakePos[%u]: %u\n",i, snakePos[i]);
+//	}	
+	if(direction == LEFT){
+		snakePos[0] -= 1;
+	}
+	if(direction == RIGHT){
+		snakePos[0] += 1;
+	}
+	if(direction == UP){
+		snakePos[0] -= 32;
+	}
+	if(direction == DOWN){
+		snakePos[0] += 32;
+	}
+}
+
+// draw new head and delete old tail
+void drawSnake(void){
+	uint16_t x = snakePos[0]%32;
+	uint16_t y = snakePos[0]/32;
+	drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, WHITE);
+	x = prevtailPos%32;
+	y = prevtailPos/32;
+	drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, BLACK);	
+}
+
+// collision detection and handling
+void collisionDetect(void){
+	uint8_t x = snakePos[0]%32;
+	uint8_t y = snakePos[0]/32;
+	if((foodx == x) && (foody == y)){
+		printf("Food eaten bitch!\n");
+		map[foodx][foody] = 0;
+		foodEaten = 1;
+		snakeLen++;
+	}
+}
+
+// init snake
+void initSnake(void){
 	
-	// init snake
-	void initSnake(void){
+	memset(map, 0, sizeof map);	
 	placeSnake();
 	drawBackground(BLACK);
 	drawText("Snake###############", 0, 0);
 	drawText("##########Score: 000", 160, 0);	
 	// draw the initial snake
-		for(int y = 0; y < MAP_HEIGHT; y++){
-			for(int x = 0; x < MAP_WIDTH; x++){
-				if(map[x][y] == 1){
-					drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, WHITE);
-				}
-				if(map[x][y] == 0){
-					drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, BLACK);
-				}
+	for(int y = 0; y < MAP_HEIGHT; y++){
+		for(int x = 0; x < MAP_WIDTH; x++){
+			if(map[x][y] == 1){
+				drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, WHITE);
 			}
-		}	
-	}
-	
-	// draw new head and delete old tail
-	void drawSnake(void){
-		uint16_t x = snakePos[0]%32;
-		uint16_t y = snakePos[0]/32;
-		drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, WHITE);
-		x = prevtailPos%32;
-		y = prevtailPos/32;
-		drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, BLACK);	
+			if(map[x][y] == 0){
+				drawRectangle(x*10, (y*10)+10, (x*10)+9, (y*10)+19, BLACK);
+			}
+		}
 	}	
+}
 
-	// function for moving snake	
-	void moveSnake(uint8_t direction){
-		
-		prevtailPos = snakePos[snakeLen-1];
+int main(int argc, char *argv[])
+{
+	printf("Hello Overlord, input and display test\n");
 
-		for(int i = MAP_SIZE; i > 0; i--){
-			if(i < snakeLen){
-				snakePos[i] = snakePos[i-1]; 	
-			}
-			else{
-				snakePos[i] = 0;
-			}
-		}
-//		for(int i=0;i<snakeLen;i++){
-//			printf("snakePos[%u]: %u\n",i, snakePos[i]);
-//		}	
-		if(direction == LEFT){
-			snakePos[0] -= 1;
-		}
-		if(direction == RIGHT){
-			snakePos[0] += 1;
-		}
-		if(direction == UP){
-			snakePos[0] -= 32;
-		}
-		if(direction == DOWN){
-			snakePos[0] += 32;
-		}
-	}
-
-	// collision detection and handling
-	void collisionDetect(void){
-		uint8_t x = snakePos[0]%32;
-		uint8_t y = snakePos[0]/32;
-		if((foodx == x) && (foody == y)){
-			printf("Food eaten bitch!\n");
-			map[foodx][foody] = 0;
-			foodEaten = 1;
-			snakeLen++;
-		}
-	}
+	srand(time(NULL));	
 	
 	displayInit();
 	initSnake();
